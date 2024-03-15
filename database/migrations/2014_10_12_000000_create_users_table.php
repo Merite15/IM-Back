0bb89dcd47382d5facd05857f7970e8c5a07c527
merite15 <meritekioungou1@gmail.com>
2024-03-15 14:54:09 +0100

    customer crud done

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -2,11 +2,13 @@
 
 declare(strict_types=1);
 
+use App\Enums\UserGender;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class () extends Migration {
+return new class() extends Migration
+{
     /**
      * Run the migrations.
      */
@@ -16,8 +18,12 @@ return new class () extends Migration {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->enum('gender', UserGender::values());
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->timestamp('last_login_at')->nullable();
+            $table->string('last_login_ip')->nullable();
+            $table->softDeletes();
             $table->rememberToken();
             $table->softDeletes();
             $table->timestamps();
--- database/migrations/2023_04_30_150318_create_customers_table.php
+++ database/migrations/2023_04_30_150318_create_customers_table.php
@@ -2,11 +2,13 @@
 
 declare(strict_types=1);
 
+use App\Enums\UserGender;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class () extends Migration {
+return new class() extends Migration
+{
     /**
      * Run the migrations.
      */
@@ -14,10 +16,13 @@ return new class () extends Migration {
     {
         Schema::create('customers', function (Blueprint $table): void {
             $table->id();
-            $table->string('name')->nullable();
-            $table->string('email')->unique()->nullable();
-            $table->string('phone')->unique()->nullable();
-            $table->string('address')->nullable();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('phone')->unique();
+            $table->string('address');
+            $table->enum('gender', UserGender::values());
+            $table->string('shop_name')->nullable();
+            $table->string('city')->nullable();
             $table->softDeletes();
             $table->timestamps();
         });
