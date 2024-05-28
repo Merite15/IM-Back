3e91c3d069528a13a5149891094324ec919b42e3
merite15 <meritekioungou1@gmail.com>
2024-05-28 08:40:46 +0100

    add sales

---
+++ database/migrations/2024_05_28_073925_create_sales_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Enums\PaymentType;
+use App\Models\Company;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sales', function (Blueprint $table): void {
+            $table->comment('Table qui stocke les ventes en magasin');
+            $table->id();
+            $table->date('date')->comment('Date de la vente');
+            $table->string('receipt_no')->comment('Numéro de reçu');
+            $table->integer('total_amount')->comment('Montant total de la vente');
+            $table->enum('payment_type', PaymentType::values())->comment('Type de paiement');
+            $table->foreignIdFor(Company::class)->constrained()->comment('Identifiant de la société associée');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sales');
+    }
+};
---
+++ database/migrations/2024_05_28_073933_create_sale_details_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\Company;
+use App\Models\Product;
+use App\Models\Sale;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sale_details', function (Blueprint $table): void {
+            $table->comment('Table qui stocke les détails des ventes en magasin');
+            $table->id();
+            $table->integer('quantity')->comment('Quantité vendue');
+            $table->integer('unit_price')->comment('Prix unitaire');
+            $table->integer('total')->comment('Total');
+            $table->foreignIdFor(Sale::class)->constrained();
+            $table->foreignIdFor(Product::class)->constrained();
+            $table->foreignIdFor(Company::class)->constrained();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sale_details');
+    }
+};
